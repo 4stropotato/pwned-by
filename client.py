a692f369354fd7af80c531b4d573edc2d856e4a1
4stropotato <69370957+4stropotato@users.noreply.github.com>
2021-09-24 21:30:10 +0900

    Getting the server's ip ( Your own machine )

---
+++ client.py
@@ -0,0 +1,16 @@
+# Day 1
+# Notice that it has the same code. Yes, because we are initializing the code to with the same ip address to communicate to each other
+
+import socket
+
+def ip_address():
+    global server_ip
+    serv_ip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serv_ip.connect(("8.8.8.8", 80))
+    server_ip = serv_ip.getsockname()[0]
+    serv_ip.close()
+    return server_ip
+
+server_ip = ip_address()
+
+print(server_ip)
