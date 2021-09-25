fe8d0f5d082b4546c2c1fc9c8fa9e3e3f430503b
4stropotato <69370957+4stropotato@users.noreply.github.com>
2021-09-26 00:17:03 +0900

    Add files via upload
    Day 2 - The Socket Module -- Connecting to the client.

---
+++ versions/v0.2/client.py
@@ -0,0 +1,24 @@
+# Day 2
+# Nothing much will happen here.
+
+import socket
+
+def ip_address():
+    global server_ip
+    serv_ip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serv_ip.connect(("8.8.8.8", 80))
+    server_ip = serv_ip.getsockname()[0]
+    serv_ip.close()
+    return server_ip
+
+########################
+
+server_ip = ip_address() # v0.2 Should be the same ip and port as the server.py
+server_port = 5555 # v0.2 
+
+########################
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # concatinating ip and tcp socket
+
+# since the server is now 
+client.connect() # >>>>>>>>>>>>>>> initiate 3 way handshake to the servere, returning back to server.py >>>>>>>>>>>> 
---
+++ versions/v0.2/server.py
@@ -0,0 +1,28 @@
+# Day 2 The socket module
+# As you noticed, we already used socket module as on the first day. The socket module is the core module that we are using since this course is more about networking.
+
+import socket
+
+def ip_address():
+    global server_ip
+    serv_ip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    serv_ip.connect(("8.8.8.8", 80))
+    server_ip = serv_ip.getsockname()[0]
+    serv_ip.close()
+    return server_ip
+
+########################
+
+server_ip = ip_address() # we can change the server ip to any ip from our machine. ie. 192.168.1.8
+server_port = 5555 # v0.2 We can choose any available ports on 65535 or 1111111111111111 in binary
+
+########################
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # We're going to concatenate our IPv4 (AF_INET) to our Transmission Control Protocol socket (SOCK_STREAM) or TCP. By the way (SOCK_DGRAM) is User Defind Protocol socket or UDP that is less reliable that TCP. 
+server.bind((server_ip,server_port)) # binding local end port's number to the ip
+
+# The server's port is open, we can now listen the the incoming connection, we can now listen to the incoming connections.
+server.listen(5) # 5 is queue size through the parameter backlog. Let's go to the client.py >>>>>>>>>>
+client, ip = server.accept() # >>>>>>>>>>>>> coming back to this file to accept the connection request from the client.py that gives 2 variables; client = connection data, and ip = returns tuple of ip of the client and port
+
+# Now we received the ip and port, we can now start hacking the system!
