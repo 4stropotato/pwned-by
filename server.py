d4dd33459b990decf1537e902275db439c0cc873
4stropotato <69370957+4stropotato@users.noreply.github.com>
2021-09-26 00:50:09 +0900

    v0.02
    Day 2 - The Socket Module -- Connecting to the client.

--- server.py
+++ server.py
@@ -25,4 +25,5 @@ server.bind((server_ip,server_port)) # binding local end port's number to the ip
 server.listen(5) # 5 is queue size through the parameter backlog. Let's go to the client.py >>>>>>>>>>
 client, ip = server.accept() # >>>>>>>>>>>>> coming back to this file to accept the connection request from the client.py that gives 2 variables; client = connection data, and ip = returns tuple of ip of the client and port
 
-# Now we received the ip and port, we can now start hacking the system! pwning tomorrow!
+# Now we received the ip and port, we can now start hacking the system!
+# pwning tmrw!
