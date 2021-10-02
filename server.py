9b10b8ab68c093b7e512ef12eb9d61643ffc7f55
shinji tanimura <drmrs.nvr.qt@gmail.com>
2021-10-02 19:47:07 +0900

    v0.31s
    Day 5 - Sending message (Client), Receiving (Server)

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 # Day 5 Sending message (Client), Receiving (Server)
-# Today, we are going to start a conversation within the connection we had made. To make it more understandable, we will move to the client.py to make a message_send function. >>>>>>>>>>>>>>>>1
+#1 Today, we are going to start a conversation within the connection we had made. To make it more understandable, we will move to the client.py to make a message_send function. 
 
 import socket
 
@@ -18,15 +18,15 @@ server_port = 5555
 
 #########################
 
-# 2>>>>>>>>>>>>>> receiving the message. since the clienct sent message twice, we will be receiving a message twice as well. One for the header, the other one for the body or the actual message.
-def message_recv(size): # new function. message_recv with size 8
-    while True: # infinite loop until we confirm that we have received the header.
+# 11 receiving the message. since the clienct sent message twice, we will be receiving a message twice as well. One for the header, the other one for the body or the actual message.
+def message_recv(size): # 12
+    while True: # 13
         header = client.recv(size).decode('utf-8')
-        if header: # if we are not receiving a message it will automatically give as false, but if the header has a value, then it will be turning True
-            body_size = int(header) # received bytes with white spaces b'5       '. if we convert it to int it will be going to 5. and that 5 will be the new size for the body
-            body = client.recv(body_size).decode('utf-8') # the body size for the body is the size that we gave from the header.
-            break # after a sucessful conversation we will break the loop.
-    return body # returning the body's value
+        if header: # 14
+            body_size = int(header) # 15
+            body = client.recv(body_size).decode('utf-8') # 16
+            break # 17
+    return body # 18
 
 def server_conn():
     global server,client,ip
@@ -47,4 +47,18 @@ print(message_recv(2))
 print(message_recv(2))
 
 print(message_recv(2))
+
+
+#########################
+
+# 12 new function. message_recv with size 8
+# 13 infinite loop until we confirm that we have received the header so we can assure that both codes will execute at the same time.
+# 14 if we are not receiving a message it will automatically give as false, but if the header has a value, then it will be turning True
+# 15 received bytes with white spaces b'5       '. if we convert it to int it will be going to 5. and that 5 will be the new size for the body
+# 16 the body size for the body is the size that we gave from the header.
+# 17 after a sucessful conversation we will break the loop.
+# 18 returning the body's value
+
+#########################
+
 # pwning tmrw!
