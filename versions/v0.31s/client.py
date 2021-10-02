84fb07d5a9d50c1f4bb003c8fb78aa644fb9e166
shinji tanimura <drmrs.nvr.qt@gmail.com>
2021-10-02 19:49:19 +0900

    v0.31s
    Day 5 - Sending message (Client), Receiving (Server)

--- versions/v0.31s/client.py
+++ versions/v0.31s/client.py
@@ -19,16 +19,16 @@ server_port = 5555
 ########################
 
 
-# 1>>>>>>>>>>>>> making message_send function.
+# 2 making message_send function.
 
-def message_send(string, size): # new function with param string(message), and the size of bytes so that the server will know how much packet it has to receive.
-    body = string.encode('utf-8') # we have to convert it to bytes so that every machine would understand the message.
-    length = len(body) # counting the length of the string and,
-    header = str(length).encode('utf-8') # converting it again to bytes. ie. 'string' has a length of 5 and converting 5 into a byte, turning b'5' 
-    header += b' ' * (size - len(header)) # adding spaces to the filler, ie. we gave the args 'string' as the string and 8 as the size. the length of string (b'5') is 1, so 8 - 1 = 7, we will make seven white spaces in bytes and add it to the filler. b'5       ' <- this has 7 white spaces 
-    client.send(header) # as we gave 8 bytes for the size, we will send an 8 bytes message to the server b'5       '. We will send the filler first as the header so that the server will know how many packets it will be going to receive next time.
-    client.send(body) # now we will send the actual message.
-    # let's go to the server.py >>>>>>>>>>>>>>>>2
+def message_send(string, size): # 3
+    body = string.encode('utf-8') # 4
+    length = len(body) # 5
+    header = str(length).encode('utf-8') # 6
+    header += b' ' * (size - len(header)) # 7
+    client.send(header) # 8
+    client.send(body) # 9
+    # 10 let's go to the server.py
 
 
 def client_conn():
@@ -45,5 +45,22 @@ message_send('Connected!', 2)
 message_send('4stropotato', 2)
 
 message_send('If you were able to print this from the server, You are awesome!', 2)
+
+
+
+########################
+
+# 3 new function with param string(message), and the size of bytes so that the server will know how much packet it has to receive.
+# 4 we have to convert it to bytes so that every machine would understand the message.
+# 5 counting the length of the string and,
+# 6 converting it again to bytes. ie. 'string' has a length of 5 and converting 5 into a byte, turning b'5' 
+# 7 adding spaces to the filler, ie. we gave the args 'string' as the string and 8 as the size.
+#   the length of string (b'5') is 1, so 8 - 1 = 7,
+#   we will make seven white spaces in bytes and add it to the filler. (b'5       ') <- this has 7 white spaces
+# 8 as we gave 8 bytes for the size, we will send an 8 bytes message to the server (b'5       ').
+#   We will send the filler first as the header so that the server will know how many packets it will be going to receive next time.
+# 9 now we will send the actual message.
+
+########################
 
 # pwning tmrw!
