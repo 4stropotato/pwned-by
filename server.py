d1bf212ec2ecda5bca509e76ac09778e8a3d15b4
4stropotato <69370957+4stropotato@users.noreply.github.com>
2021-10-02 13:51:29 +0900

    v0.31s
    Day 5 - Sending message (Client), Receiving (Server)

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-# Day 4 Cleaning the code
-# As a programmer, it is very important to make a clean code. (Readable and Organized) So it is important that we arrange our code according to it's functionality. 
-# Header sned/receive
+# Day 5 Sending message (Client), Receiving (Server)
+# Today, we are going to start a conversation within the connection we had made. To make it more understandable, we will move to the client.py to make a message_send function. >>>>>>>>>>>>>>>>1
 
 import socket
 
@@ -19,9 +18,17 @@ server_port = 5555
 
 #########################
 
+# 2>>>>>>>>>>>>>> receiving the message. since the clienct sent message twice, we will be receiving a message twice as well. One for the header, the other one for the body or the actual message.
+def message_recv(size): # new function. message_recv with size 8
+    while True: # infinite loop until we confirm that we have received the header.
+        header = client.recv(size).decode('utf-8')
+        if header: # if we are not receiving a message it will automatically give as false, but if the header has a value, then it will be turning True
+            body_size = int(header) # received bytes with white spaces b'5       '. if we convert it to int it will be going to 5. and that 5 will be the new size for the body
+            body = client.recv(body_size).decode('utf-8') # the body size for the body is the size that we gave from the header.
+            break # after a sucessful conversation we will break the loop.
+    return body # returning the body's value
 
-
-def server_conn(): # new function # Since we only have few codes yet, we can put all the remaining code in a container where we can call to make a connection.
+def server_conn():
     global server,client,ip
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((server_ip,server_port))
@@ -32,11 +39,12 @@ def server_conn(): # new function # Since we only have few codes yet, we can put
     #####   DELETABLE   #####
     client, ip = server.accept()
 
-def message_recv(): # new function
-    confirmation = client.recv(8).decode('utf-8')
-    print(confirmation)
-
 server_conn()
-message_recv()
 
+# testing
+print(message_recv(2))
+
+print(message_recv(2))
+
+print(message_recv(2))
 # pwning tmrw!
