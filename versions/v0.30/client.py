73474c91541f43a825ba5ea6c6da092089c5d87c
shinji tanimura <drmrs.nvr.qt@gmail.com>
2021-10-02 23:44:48 +0900

    v0.30
    Day 4 - Cleaning the code + sending message from client to server

--- versions/v0.30/client.py
+++ versions/v0.30/client.py
@@ -1,4 +1,4 @@
-# # Day 4 Cleaning the code
+# Day 4 - Cleaning the code + sending message from client to server
 
 import socket
 
@@ -18,16 +18,25 @@ server_port = 5555
 ########################
 
 
-def client_conn(): # new function
+def client_conn(): # 4
     global client
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
     client.connect((server_ip,server_port))
 
-def message_send(): # new function
-    confirmation = 'Connect!'.encode('utf-8')
-    client.send(confirmation)
+def message_send(): # 5
+    confirmation = 'Connect!'.encode('utf-8') # 6
+    client.send(confirmation) # 7
 
 client_conn()
 message_send()
+
+#########################
+
+# 4     Cleaning
+# 5     Sending message to the server
+# 6     We will be encoding the string ('Connect!') to utf-8 first and then...
+# 7     We are going to send it to the client variable. Also given from (client = socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+
+#########################
 
 # pwning tmrw!
--- versions/v0.30/server.py
+++ versions/v0.30/server.py
@@ -1,6 +1,5 @@
-# Day 4 Cleaning the code
-# As a programmer, it is very important to make a clean code. (Readable and Organized) So it is important that we arrange our code according to it's functionality. 
-# Header sned/receive
+# Day 4 - Cleaning the code + sending message from client to server
+#       1 As a programmer, it is very important to make a clean code. (Readable and Organized) So it is important that we arrange our code according to it's functionality. 
 
 import socket
 
@@ -21,18 +20,26 @@ server_port = 5555
 
 
 
-def server_conn(): # new function # Since we only have few codes yet, we can put all the remaining code in a container where we can call to make a connection.
+def server_conn(): # 1
     global server,client,ip
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((server_ip,server_port))
     server.listen(5)
     client, ip = server.accept()
 
-def message_recv(): # new function
-    confirmation = client.recv(8).decode('utf-8')
+def message_recv(): # 2
+    confirmation = client.recv(8).decode('utf-8') # 3
     print(confirmation)
 
 server_conn()
 message_recv()
+
+#########################
+
+# 1     Cleaning # Since we only have few codes yet, we can put all the remaining code in a container where we can call to make a connection.
+# 2     Receiving message from client
+# 3     from client that were given from (client, ip = server.accept()), we will be receiving 8 bytes from the client and we will be decoding it.
+
+#########################
 
 # pwning tmrw!
